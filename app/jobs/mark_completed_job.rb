f4aa13d3324c071d3ff90f12c81fc3125c29c2ee
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-10-11 12:41:21 -0700

    fix query

--- app/jobs/mark_completed_job.rb
+++ app/jobs/mark_completed_job.rb
@@ -2,7 +2,7 @@ class MarkCompletedJob < ApplicationJob
   queue_as :default
 
   def perform
-    @appointments.
+    Appointment.all.
       where(status: :approved).
       where('requested_datetime < ?', Time.zone.now.to_datetime)
 
